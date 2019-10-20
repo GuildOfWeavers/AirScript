c0d484c8bdeaf64effdc013cfff941cdcbbd839c
bobbinth <bobbinth@protonmail.com>
2019-10-20 14:00:20 -0700

    minor updates to assembly generator

--- lib/generators/asGenerator.ts
+++ lib/generators/asGenerator.ts
@@ -6,6 +6,36 @@ import { ScriptSpecs } from "../ScriptSpecs";
 // PUBLIC FUNCTIONS
 // ================================================================================================
 export function generateAssembly(specs: ScriptSpecs, limits: StarkLimits, extensionFactor: number): string {
+        
+    // name and field
+    const name = `(name ${specs.name})\n`;
+    const field = `(field prime ${(specs.field as any).modulus})\n`;
+
+    // global constants
+    let constants = '';
+    for (let [name, expression] of specs.globalConstants) {
+        constants += `(const ${expression.toAssembly()})\n`;
+    }
+
+    // inputs
+    let inputs = '';
+    for (let register of specs.transitionFunction.inputRegisterSpecs) {
+        inputs += `(input rank ${register})\n`;
+    }
+
+    // transition function and constraints
     const tFunction = specs.transitionFunction.toAssembly();
-    return tFunction;
+    const tConstraints = specs.transitionConstraints.toAssembly();
+
+    // static section
+    let kRegisters = '';
+    if (specs.staticRegisters.length > 0) {
+        for (let register of specs.staticRegisters) {
+            let binary = register.binary ? ' binary' : '';
+            kRegisters += `  (${register.pattern}${binary} (${register.values.join(' ')}))\n`;
+        }
+        kRegisters = `(static\n${kRegisters})`;
+    }
+
+    return `(stark ${name}${field}${constants}${inputs}${tFunction}${tConstraints}${kRegisters})`;
 }
